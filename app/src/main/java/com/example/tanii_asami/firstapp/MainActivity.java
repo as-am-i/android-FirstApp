5395ca2db7632bcf3460cb105a20460ce0b44a24
asamitanii <g033.9521@gmail.com>
2018-01-11 14:31:03 -0800

    Chapter3

--- app/src/main/java/com/example/tanii_asami/firstapp/MainActivity.java
+++ app/src/main/java/com/example/tanii_asami/firstapp/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.tanii_asami.firstapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final String KEY_INDEX = "index";
     
     private Button mTrueButton;
     private Button mFalseButton;
@@ -29,30 +33,19 @@ public class MainActivity extends AppCompatActivity {
             new Question(R.string.question_frog9, true),
     };
 
-    private void updateQuestion() {
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestinTextView.setText(question);
-    }
-
-    private void checkAnswer(boolean userPreseedTrue) {
-        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-        // isAnswerTrue needs an instance to call it
-
-        int messageResId = 0;
-
-        if (userPreseedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
-        } else {
-            messageResId = R.string.wrong_toast;
-        }
-
-        Toast.makeText(this, messageResId, Toast.LENGTH_LONG).show();
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
+
+        // check if savedInstanceState exist in onCreate() method
+        // (saving Data Acriss Rotation)
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
+
 
         // get a reference for the TextView and set its text to the question at the current index
         mQuestinTextView = (TextView) findViewById(R.id.question_text_view); // id in the view
@@ -63,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
-        
+
         mTrueButton = (Button) findViewById(R.id.trueButton);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,5 +92,63 @@ public class MainActivity extends AppCompatActivity {
         });
 
         updateQuestion(); // to initially set the text in the activity's view
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestinTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPreseedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        // isAnswerTrue needs an instance to call it
+
+        int messageResId = 0;
+
+        if (userPreseedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.wrong_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_LONG).show();
+    };
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() caleed");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
